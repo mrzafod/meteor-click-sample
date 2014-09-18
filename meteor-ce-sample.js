6c078ba3bb5670cfa7c56829d4df50549fab0eb4
roman <isbscompany@gmail.com>
2014-09-19 02:37:36 +0400

    +++

---
+++ meteor-ce-sample.js
@@ -0,0 +1,50 @@
+
+Messages = new Meteor.Collection('Messages');
+
+if(Meteor.isClient) {
+	Template.messages.created = function () {
+		this.sub = Meteor.subscribe('messages');
+	}
+
+	Template.messages.helpers({
+		messages: function () {
+			return Messages.find({});
+		}
+	});
+
+
+	Template.message.rendered = function () {
+		this.input = this.find('[contenteditable=true]');
+
+		this.autorun((function(self) {
+			function() {
+				self.input.innerText = (Messages.findOne(self.data._id) || {}).body;
+			};
+		})(this));
+	}
+
+	Template.message.events({
+		'click [action=edit]': function (event, tmpl) {
+			event.preventDefault();
+			Messages.update(tmpl.data._id, {$set: {body: tmpl.input.innerText}})
+		}
+	})
+
+	Template.newMessage.rendered = function () {
+		this.input = this.firstNode
+	}	
+
+	Template.newMessage.events({
+		'click [action=send]': function (event, tmpl) {
+			event.preventDefault();
+			Messages.insert({body: tmpl.input.innerText});
+			tmpl.input.innerText = '';
+		}
+	})
+}
+
+if(Meteor.isServer) {
+	Meteor.publish('messages', function () {
+		return Messages.find({})
+	})
+}
